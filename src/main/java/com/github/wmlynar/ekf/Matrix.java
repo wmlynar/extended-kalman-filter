69cdf353a0ecb529627f2fd972a8c75fdb02c04b
wmlynar <wojciech.mlynarczyk+github@gmail.com>
2017-08-29 09:38:36 +0200

    added zero_matrix function

--- src/main/java/com/github/wmlynar/ekf/Matrix.java
+++ src/main/java/com/github/wmlynar/ekf/Matrix.java
@@ -281,4 +281,12 @@ public class Matrix {
 
 		return true;
 	}
+	
+	public void zero_matrix() {
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				data[i][j] = 0.0;
+			}
+		}
+	}
 }
