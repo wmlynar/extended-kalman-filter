01fc0a95ad6c652a3407e1898562e87a247c3496
wmlynar <wojciech.mlynarczyk+github@gmail.com>
2017-08-29 19:24:26 +0200

    renamed parameters in model

--- src/main/java/com/github/wmlynar/dekf/examples/SpeedAngleDProcessModel.java
+++ src/main/java/com/github/wmlynar/dekf/examples/SpeedAngleDProcessModel.java
@@ -26,19 +26,19 @@ public class SpeedAngleDProcessModel extends DProcessModel {
 	}
 
 	@Override
-	public void stateFunction(Matrix state, Matrix state_delta) {
-		state_delta.data[0][0] = state.data[2][0] * Math.sin(state.data[3][0]);
-		state_delta.data[1][0] = state.data[2][0] * Math.cos(state.data[3][0]);
-		state_delta.data[2][0] = 0;
-		state_delta.data[3][0] = 0;
+	public void stateFunction(Matrix state, Matrix function) {
+		function.data[0][0] = state.data[2][0] * Math.sin(state.data[3][0]);
+		function.data[1][0] = state.data[2][0] * Math.cos(state.data[3][0]);
+		function.data[2][0] = 0;
+		function.data[3][0] = 0;
 	}
 
 	@Override
-	public void stateFunctionJacobian(Matrix state, Matrix state_delta_jacobian) {
-		state_delta_jacobian.data[0][2] = Math.sin(state.data[3][0]);
-		state_delta_jacobian.data[0][3] = state.data[2][0] * Math.cos(state.data[3][0]);
-		state_delta_jacobian.data[1][2] = Math.cos(state.data[3][0]);
-		state_delta_jacobian.data[1][3] = -state.data[2][0] * Math.sin(state.data[3][0]);
+	public void stateFunctionJacobian(Matrix state, Matrix function_jacobian) {
+		function_jacobian.data[0][2] = Math.sin(state.data[3][0]);
+		function_jacobian.data[0][3] = state.data[2][0] * Math.cos(state.data[3][0]);
+		function_jacobian.data[1][2] = Math.cos(state.data[3][0]);
+		function_jacobian.data[1][3] = -state.data[2][0] * Math.sin(state.data[3][0]);
 	}
 
 	@Override
