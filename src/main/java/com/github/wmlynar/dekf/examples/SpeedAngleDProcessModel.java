53bcbbcb60abd6431618aeaf897776c7e2470cc7
wmlynar <wojciech.mlynarczyk+github@gmail.com>
2017-08-29 17:45:09 +0200

    added angle test for dkalman

---
+++ src/main/java/com/github/wmlynar/dekf/examples/SpeedAngleDProcessModel.java
@@ -0,0 +1,49 @@
+package com.github.wmlynar.dekf.examples;
+
+import com.github.wmlynar.ctekf.CtProcessModel;
+import com.github.wmlynar.dekf.DProcessModel;
+import com.github.wmlynar.ekf.Matrix;
+
+public class SpeedAngleDProcessModel extends DProcessModel {
+
+	@Override
+	public int stateDimension() {
+		return 4;
+	}
+
+	@Override
+	public void initialState(Matrix initial_state) {
+		initial_state.data[0][0] = 0;
+		initial_state.data[1][0] = 0;
+		initial_state.data[2][0] = 0;
+		initial_state.data[3][0] = 0;
+	}
+
+	@Override
+	public void initialStateCovariance(Matrix initial_covariance) {
+		initial_covariance.set_identity_matrix();
+		initial_covariance.scale_matrix(1000);
+	}
+
+	@Override
+	public void predictionModel(Matrix state, Matrix state_delta) {
+		state_delta.data[0][0] = state.data[2][0] * Math.sin(state.data[3][0]);
+		state_delta.data[1][0] = state.data[2][0] * Math.cos(state.data[3][0]);
+		state_delta.data[2][0] = 0;
+		state_delta.data[3][0] = 0;
+	}
+
+	@Override
+	public void predictionModelJacobian(Matrix state, Matrix state_delta_jacobian) {
+		state_delta_jacobian.data[0][2] = Math.sin(state.data[3][0]);
+		state_delta_jacobian.data[0][3] = state.data[2][0] * Math.cos(state.data[3][0]);
+		state_delta_jacobian.data[1][2] = Math.cos(state.data[3][0]);
+		state_delta_jacobian.data[1][3] = -state.data[2][0] * Math.sin(state.data[3][0]);
+	}
+
+	@Override
+	public void processNoiseCovariance(Matrix process_noise_covariance) {
+		process_noise_covariance.set_identity_matrix();
+	}
+
+}
---
+++ src/test/java/com/github/wmlynar/dekf/examples/AngleSpeedCtModelTest.java
@@ -0,0 +1,38 @@
+package com.github.wmlynar.dekf.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.wmlynar.dekf.DKalmanFilter;
+
+public class AngleSpeedCtModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		SpeedAngleDProcessModel model = new SpeedAngleDProcessModel();
+		SpeedAngleDObservationModel obs = new SpeedAngleDObservationModel();
+		DKalmanFilter filter = new DKalmanFilter(model);
+		
+        for (int i = 0; i <= 10; ++i) {
+        	obs.setPosition(i,i);
+            filter.update(1,obs);
+        }
+        
+        double x = filter.model.state_estimate.data[0][0];
+        double y = filter.model.state_estimate.data[1][0];
+        double v = filter.model.state_estimate.data[2][0];
+        double alpha = filter.model.state_estimate.data[3][0];
+  
+        assertEquals(10,x,1e-3);
+        assertEquals(10,y,1e-3);
+        assertEquals(Math.sqrt(2),v,1e-3);
+        assertEquals(Math.PI/4,alpha,1e-3);
+	}
+
+}
