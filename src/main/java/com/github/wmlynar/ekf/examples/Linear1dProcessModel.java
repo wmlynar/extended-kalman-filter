197e70dcb103da6f8d4bea823222c5c5a34fa508
wmlynar <wojciech.mlynarczyk+github@gmail.com>
2017-08-29 22:41:33 +0200

    updated example to be more readable and be presented in readme file

--- src/main/java/com/github/wmlynar/ekf/examples/Linear1dProcessModel.java
+++ src/main/java/com/github/wmlynar/ekf/examples/Linear1dProcessModel.java
@@ -18,22 +18,30 @@ public class Linear1dProcessModel extends ProcessModel {
 	@Override
 	public void initialStateCovariance(double[][] cov) {
 		cov[0][0] = 1000;
+		cov[0][1] = 0;
+		cov[1][0] = 0;
 		cov[1][1] = 1000;
 	}
 
 	@Override
 	public void stateFunction(double[][] x, double[][] f) {
 		f[0][0] = x[1][0];
+		f[1][0] = 0;
 	}
 
 	@Override
 	public void stateFunctionJacobian(double[][] x, double[][] j) {
+		j[0][0] = 0;
 		j[0][1] = 1;
+		j[1][0] = 0;
+		j[1][1] = 0;
 	}
 
 	@Override
 	public void processNoiseCovariance(double[][] cov) {
 		cov[0][0] = 1;
+		cov[0][1] = 0;
+		cov[1][0] = 0;
 		cov[1][1] = 1;
 	}
 }
